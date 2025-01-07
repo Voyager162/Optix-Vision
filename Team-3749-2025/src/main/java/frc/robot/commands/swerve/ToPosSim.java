5346e919b3f16462ce43beb79e72d98d3fa7b778
Harkirat47 <har.ki.kirat@gmail.com>
2025-01-06 21:57:13 -0800

    done with a plan on the command base

---
+++ Team-3749-2025/src/main/java/frc/robot/commands/swerve/ToPosSim.java
@@ -0,0 +1,19 @@
+package frc.robot.commands.swerve;
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.subsystems.swerve.Swerve;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ToPosSim {
+/**
+ * Im pretty sure this is where I actually run the trajectories. for that Ineed to figure out two things.
+ * 1: is the swerve drive controller OBJECT(Hark- hopefully by end of ENM),
+ * 2: is physically running with it (- John and Hark at meeting)
+ * 3: is constants to endpoints to see it working in sim( this takes like an minute, but remember to do it correctly)
+ * 4: is obvios, robot container button bindings (This is where hopefully it updates states with a push of a button so no need for a seperate sim file, 
+ * but if we need one, we can see and it will just go here after this step )
+ * 5: SUFFERING to debug it all (Both Hark an John)
+ */
+
+}
