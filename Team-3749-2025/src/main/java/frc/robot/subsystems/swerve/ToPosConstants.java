9ba2d0e115589bc40f57d926b7dd5657a7ba1f61
spooketti <jonliu168@gmail.com>
2025-01-09 17:01:52 -0800

    pose toggler

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
@@ -15,7 +15,7 @@ public class ToPosConstants {
     public static final class Setpoints
     {
         public static final Pose2d coralTop = new Pose2d(1.21, 7.0, new Rotation2d(Math.toRadians(120)));
-        public static final Pose2d coralBottom = new Pose2d(1.21,1.15,new Rotation2d(Math.toRadians(50)));
+        public static final Pose2d coralBottom = new Pose2d(1.21,1.15,new Rotation2d(Math.toRadians(231)));
         public static final Pose2d processor = new Pose2d(11.3,7.3,new Rotation2d(Math.toRadians(93.75)));
         public static final Pose2d reef18 = new Pose2d(3.09,4.03,new Rotation2d(Math.toRadians(0)));
         public static final Pose2d reef17 = new Pose2d(3.7,2.85,new Rotation2d(Math.toRadians(60)));
