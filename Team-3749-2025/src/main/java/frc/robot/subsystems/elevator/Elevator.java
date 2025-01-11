f63eec5c9c9dd1a2a4b47afbfc6faa6f7e2fe8b8
Dhyan Soni <dhyantsoni@email.com>
2025-01-11 11:39:38 -0800

    private vars and organizatoin

--- Team-3749-2025/src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -17,32 +17,32 @@ import frc.robot.utils.UtilityFunctions;
 public class Elevator extends SubsystemBase {
     // armIO.setVoltage(pidController.calculate(getPosition(), setpoint) + ArmConstants.armControl.armkG);
     
-    ElevatorIO elevatorio;
-    ElevatorData data = new ElevatorData();
+    private ElevatorIO elevatorio;
+    private ElevatorData data = new ElevatorData();
     private ElevatorStates state = ElevatorStates.STOP;
     private ElevatorConstants constants = new ElevatorConstants();
-    double setpoint;
-
-    PIDController pidController = new PIDController(ElevatorConstants.ElevatorControl.kPSim, 0, ElevatorConstants.ElevatorControl.kDSim);
-    ElevatorFeedforward feedForward = new ElevatorFeedforward(ElevatorConstants.ElevatorControl.kASim, ElevatorConstants.ElevatorControl.kGSim, ElevatorConstants.ElevatorControl.kSSim, ElevatorConstants.ElevatorControl.kASim);
+    
+    private PIDController pidController = new PIDController(ElevatorConstants.ElevatorControl.kPSim, 0, ElevatorConstants.ElevatorControl.kDSim);
+    // maybe later
+    private ElevatorFeedforward feedForward = new ElevatorFeedforward(ElevatorConstants.ElevatorControl.kASim, ElevatorConstants.ElevatorControl.kGSim, ElevatorConstants.ElevatorControl.kSSim, ElevatorConstants.ElevatorControl.kASim);
 
     private ShuffleData<String> currentCommandLog = new ShuffleData<String>(this.getName(), "current command", "None");
-    public ShuffleData<Double> positionMetersLog = new ShuffleData<Double>("Elevator", "position meters", 0.0);
-    public ShuffleData<Double> velocityUnitsLog = new ShuffleData<Double>("Elevator", "velocity units", 0.0);
-    public ShuffleData<Double> accelerationUnitsLog = new ShuffleData<Double>("Elevator", "acceleration units", 0.0);
-    public ShuffleData<Double> inputVoltsLog = new ShuffleData<Double>("Elevator", "input volts", 0.0);
-    public ShuffleData<Double> appliedVoltsLog = new ShuffleData<Double>("Elevator", "applied volts", 0.0);
-    public ShuffleData<Double> currentAmpsLog = new ShuffleData<Double>("Elevator", "current amps", 0.0);
-    public ShuffleData<Double> tempCelciusLog = new ShuffleData<Double>("Elevator", "temp celcius", 0.0);
+    private ShuffleData<Double> positionMetersLog = new ShuffleData<Double>("Elevator", "position meters", 0.0);
+    private ShuffleData<Double> velocityUnitsLog = new ShuffleData<Double>("Elevator", "velocity units", 0.0);
+    private ShuffleData<Double> accelerationUnitsLog = new ShuffleData<Double>("Elevator", "acceleration units", 0.0);
+    private ShuffleData<Double> inputVoltsLog = new ShuffleData<Double>("Elevator", "input volts", 0.0);
+    private ShuffleData<Double> appliedVoltsLog = new ShuffleData<Double>("Elevator", "applied volts", 0.0);
+    private ShuffleData<Double> currentAmpsLog = new ShuffleData<Double>("Elevator", "current amps", 0.0);
+    private ShuffleData<Double> tempCelciusLog = new ShuffleData<Double>("Elevator", "temp celcius", 0.0);
 
     // Temporary?
     // private ShuffleData<Double> kPData = new ShuffleData<Double>("Elevator", "kPData", ElevatorConstants.ElevatorControl.kPSim);
     // private ShuffleData<Double> kDData = new ShuffleData<Double>("Elevator", "kDData", ElevatorConstants.ElevatorControl.kDSim); 
     // private ShuffleData<Double> kGData = new ShuffleData<Double>("Elevator", "kGData", ElevatorConstants.ElevatorControl.kGSim); 
 
-    public static Mechanism2d mech = new Mechanism2d(3, 3);
-    public static MechanismRoot2d root = mech.getRoot("elevator", 2, 0);
-    public static MechanismLigament2d elevatorMech = root.append(new MechanismLigament2d("elevator", Units.feetToMeters(3.25), 90));
+    public Mechanism2d mech = new Mechanism2d(3, 3);
+    public MechanismRoot2d root = mech.getRoot("elevator", 2, 0);
+    public MechanismLigament2d elevatorMech = root.append(new MechanismLigament2d("elevator", Units.feetToMeters(3.25), 90));
 
     public Elevator(){
         if (Robot.isSimulation()) {
@@ -74,6 +74,10 @@ public class Elevator extends SubsystemBase {
             case STOW:
                 break;
         }
+    }
+
+    private void runStateStop() {
+        elevatorio.setVoltage(0);
     }
 
     // returns true when the state is reached
@@ -119,10 +123,6 @@ public class Elevator extends SubsystemBase {
         appliedVoltsLog.set(data.appliedVolts);
         currentAmpsLog.set(data.currentAmps);
         tempCelciusLog.set(data.tempCelcius);
-    }
-
-    private void runStateStop() {
-        elevatorio.setVoltage(0);
     }
 
     public void setVoltage(double volts){
