1694f5784f9be39b36efe1d63bb7835fdd8f3fd0
Dhyan Soni <dhyantsoni@email.com>
2025-01-12 15:08:34 -0800

    organized constants file, tuned kP and kD again

--- Team-3749-2025/src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -2,23 +2,19 @@ package frc.robot.subsystems.elevator;
 
 import edu.wpi.first.math.util.Units;
 public class ElevatorConstants {
-    double l1Height = Units.inchesToMeters(18);
-    double l2Height = Units.inchesToMeters(31.875);
-    double l3Height = Units.inchesToMeters(47.625);
-    double l4Height = Units.inchesToMeters(72);
     public static class ElevatorSpecs {
         public static final double gearing = 6.0;
         public static final double carriageMassKg = 12;
         public static final double drumRadiusMeters = Units.inchesToMeters(2);
         public static final double minHeightMeters = 0;
-        public static final double maxHeightMeters = Units.feetToMeters(6);
+        public static final double maxHeightMeters = Units.feetToMeters(25); // remeasure maxV and A
         // public static final boolean simulateGravity = true;
         public static final double startingHeightMeters = 0;
     } 
 
     public static class ElevatorControl {
-        public static final double kPSim = 3;
-        public static final double kDSim = 0;
+        public static final double kPSim = 1;
+        public static final double kDSim = 0.02; // 1/20 of kP for now
         public static final double kSSim = 0;
         public static final double kGSim = 2.3;
         public static final double kVSim = 2.365; // 12 - 2.3 / 4.1 NOT USED
@@ -37,5 +33,11 @@ public class ElevatorConstants {
         L4,
         MAX,
         STOW
+    }
+    public static class stateHeights {
+        public static final double l1Height = Units.inchesToMeters(18);
+        public static final double l2Height = Units.inchesToMeters(31.875);
+        public static final double l3Height = Units.inchesToMeters(47.625);
+        public static final double l4Height = Units.inchesToMeters(72);
     }
 }
