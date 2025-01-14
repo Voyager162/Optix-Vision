216f0d8eef834efe40574e5c86c5bf10a60fd226
spooketti <jonliu168@gmail.com>
2025-01-13 21:25:22 -0800

    added processor point

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
@@ -16,7 +16,7 @@ public class ToPosConstants {
     {
         public static final Pose2d coralTop = new Pose2d(1.21, 7.0, new Rotation2d(Math.toRadians(120)));
         public static final Pose2d coralBottom = new Pose2d(1.21,1.15,new Rotation2d(Math.toRadians(231)));
-        public static final Pose2d processor = new Pose2d(11.3,7.3,new Rotation2d(Math.toRadians(93.75)));
+        public static final Pose2d processor = new Pose2d(6.3,0.66,new Rotation2d(Math.toRadians(-90)));
         public static final Pose2d reef18 = new Pose2d(3.09,4.03,new Rotation2d(Math.toRadians(0)));
         public static final Pose2d reef17 = new Pose2d(3.7,2.85,new Rotation2d(Math.toRadians(60)));
         public static final Pose2d reef22 = new Pose2d(5.24,2.75,new Rotation2d(Math.toRadians(120)));
