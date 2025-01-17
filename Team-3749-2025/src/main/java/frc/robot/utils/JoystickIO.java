df805907cee355e19678037e07ef5c56aa489b97
Dhyan Soni <91002979+XeiexGithub@users.noreply.github.com>
2025-01-16 19:26:30 -0800

    Elevator PR (#8)
    APPROVED BY NEEL
    
    * rough draft of elevator subsystem folder
    * more setup
    * added states
    * pidcontroller obj, kp/kd shuffledata, mech2d
    * put mech into periodic
    * mech2d setup
    * transfering a command to andrew
    * add setheight
    * added movetosetpoint test cmd
    * trying to get the elevator moving
    * the elevator works, so does mech2d
    * added feedforward model, tuned kG for sim, skuffed bindings but will fix later
    * PID is tuned but its sim
    * completed states
    * fixed the calculation for setHeight, everything else works
    * removed unused imports + commented unused variables
    * hashmap to store choosers, func to create the chooser
    * updated states
    * changed isStableState()
    * private vars and organizatoin
    * added button bindings to joystickIO
    * mech2d reorganization
    * made mech2d private
    * kV tuned
    * before review
    * organized functions
    * controller bindings fixed
    * organized constants file, tuned kP and kD again
    * profiledPidController actually being used now
    * removed unused imports
    * maybe fixed starting movement
    * updated tuning, transfering changes to laptop
    * tuned kv+kp
    * little more accurate kV and kP
    * made sparkmax, still a few errors
    * added logging for sparkmax
    * reorganized folders
    * remove unused imports
    * formatted, added smartCurrent constant, removed unnessesary comments
    * added simplemotorfeedforward
    * elevatorfeedfoward object
    * organization
    * reset height and removed unused + tested
    * adjusting a few more files for organization
    * deleted test cmd file
    * pr review adjust (not complete)
    * fixed a few errors
    * fix error + more pr
    * absolute encoder
    * absoluteEncoder, adjusting postition logging
    * added logging for sparkmax
    * pr review
    * Rename setElevatorState.java to SetElevatorState.java
    * added zerooffset
    * remove unused imports
    * deadband
    * deadband again
    
    ---------
    Co-authored-by: Dhyan Soni <dhyantsoni@email.com>
    Co-authored-by: andrewg5 <andrewge1911@gmail.com>

--- Team-3749-2025/src/main/java/frc/robot/utils/JoystickIO.java
+++ Team-3749-2025/src/main/java/frc/robot/utils/JoystickIO.java
@@ -5,9 +5,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Robot;
+import frc.robot.commands.elevator.SetElevatorState;
 import frc.robot.commands.example.ExampleSubsystemCommand;
 import frc.robot.commands.swerve.DriveStraight;
 import frc.robot.commands.swerve.SwerveDefaultCommand;
+import frc.robot.subsystems.elevator.ElevatorConstants.ElevatorStates;
 
 /**
  * Util class for button bindings
@@ -20,6 +22,11 @@ public class JoystickIO {
     private static final CommandXboxController pilot = new CommandXboxController(0);
     private static final CommandXboxController operator = new CommandXboxController(1);
     private static final Command DriveStraight = new DriveStraight();
+
+    private static final SetElevatorState l1 = new SetElevatorState(ElevatorStates.L1);
+    private static final SetElevatorState l2 = new SetElevatorState(ElevatorStates.L2);
+    private static final SetElevatorState l3 = new SetElevatorState(ElevatorStates.L3);
+    private static final SetElevatorState l4 = new SetElevatorState(ElevatorStates.L4);
 
     public JoystickIO() {
     }
@@ -57,6 +64,10 @@ public class JoystickIO {
         // Example binding
         operator.a().whileTrue(new ExampleSubsystemCommand());
 
+        operator.a().onTrue(l1);
+        operator.b().onTrue(l2);
+        operator.x().onTrue(l3);
+        operator.y().onTrue(l4);
     }
 
     public static void pilotBindings() {
