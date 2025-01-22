cd9f3947e57ad0e8ecc69f9ce043ff62c61af0f8
andrewg5 <andrewge1911@gmail.com>
2025-01-21 17:44:29 -0800

    changed l4 to work with l2-4

--- Team-3749-2025/src/main/java/frc/robot/commands/integration/ScoreL234.java
+++ Team-3749-2025/src/main/java/frc/robot/commands/integration/ScoreL234.java
@@ -10,19 +10,22 @@ import frc.robot.subsystems.roller.Roller;
 import frc.robot.commands.elevator.SetElevatorState;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class ScoreL4 extends Command {
+//works for l2-4 change name
+public class ScoreL234 extends Command {
     private final Chute chute;    
     private final CoralArm coralArm;
     private final Elevator elevator;
     private final Roller[] rollers;
+    private final ElevatorStates state;
     private final SetElevatorState setElevatorState;
 
-    public ScoreL4(Chute chute, CoralArm coralArm, Elevator elevator, Roller[] rollers) {
+    public ScoreL234(Chute chute, CoralArm coralArm, Elevator elevator, Roller[] rollers, ElevatorStates state) {
         this.chute = chute;
         this.coralArm = coralArm;
         this.elevator = elevator;
         this.rollers = rollers;
-        this.setElevatorState = new SetElevatorState(ElevatorStates.L4);
+        this.state = state;
+        this.setElevatorState = new SetElevatorState(this.state);
     }
 
     @Override
@@ -47,7 +50,7 @@ public class ScoreL4 extends Command {
         if (chute.hasPiece() && coralArm.getState() == CoralConstants.ArmStates.CORAL_PICKUP && coralArm.getIsStableState()) {
             CommandScheduler.getInstance().schedule(setElevatorState);
         }
-        if (chute.hasPiece() && coralArm.getState() == CoralConstants.ArmStates.CORAL_PICKUP && coralArm.getIsStableState() && elevator.getState() == ElevatorStates.L4 && elevator.getIsStableState()) {
+        if (chute.hasPiece() && coralArm.getState() == CoralConstants.ArmStates.CORAL_PICKUP && coralArm.getIsStableState() && elevator.getState() == state && elevator.getIsStableState()) {
             rollers[2].setState(RollerConstants.RollerStates.SCORE); 
         }
     }
@@ -63,6 +66,6 @@ public class ScoreL4 extends Command {
 
     @Override
     public boolean isFinished() {
-        return elevator.getState() == ElevatorStates.L4 && !chute.hasPiece();
+        return elevator.getState() == state && !chute.hasPiece();
     }
 }
