77f35d2f8e08a92e27e8c795fe7c160e91d0d865
AkhilKulkarni123 <akhilkulki1113@gmail.com>
2025-01-24 16:44:12 -0800

    Update Arm.java for private methods

--- Team-3749-2025/src/main/java/frc/robot/subsystems/arm/Arm.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -15,29 +15,29 @@ import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
  */
 public abstract class Arm extends SubsystemBase {
 
-	protected ArmIO armIO;
-	protected ArmData data = new ArmData();
-	protected ShuffleData<String> currentCommandLog = new ShuffleData<>(this.getName(), "current command", "None");
-	protected ShuffleData<Double> positionUnitsLog = new ShuffleData<>(this.getName(), "position units", 0.0);
-	protected ShuffleData<Double> velocityUnitsLog = new ShuffleData<>(this.getName(), "velocity units", 0.0);
+	private ArmIO armIO;
+	private ArmData data = new ArmData();
+	private ShuffleData<String> currentCommandLog = new ShuffleData<>(this.getName(), "current command", "None");
+	private ShuffleData<Double> positionUnitsLog = new ShuffleData<>(this.getName(), "position units", 0.0);
+	private ShuffleData<Double> velocityUnitsLog = new ShuffleData<>(this.getName(), "velocity units", 0.0);
 	public ShuffleData<Double> inputVoltsLog = new ShuffleData<Double>(this.getName(), "input volts", 0.0);
 
-	protected ShuffleData<Double> firstMotorAppliedVoltsLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> firstMotorAppliedVoltsLog = new ShuffleData<>(this.getName(),
 			"first motor applied volts", 0.0);
-	protected ShuffleData<Double> secondMotorAppliedVoltsLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> secondMotorAppliedVoltsLog = new ShuffleData<>(this.getName(),
 			"second motor applied volts", 0.0);
-	protected ShuffleData<Double> firstMotorCurrentAmpsLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> firstMotorCurrentAmpsLog = new ShuffleData<>(this.getName(),
 			"first motor current amps", 0.0);
-	protected ShuffleData<Double> secondMotorCurrentAmpsLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> secondMotorCurrentAmpsLog = new ShuffleData<>(this.getName(),
 			"second motor current amps", 0.0);
-	protected ShuffleData<Double> firstMotorTempCelciusLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> firstMotorTempCelciusLog = new ShuffleData<>(this.getName(),
 			"first motor temp celcius", 0.0);
-	protected ShuffleData<Double> secondMotorTempCelciusLog = new ShuffleData<>(this.getName(),
+	private ShuffleData<Double> secondMotorTempCelciusLog = new ShuffleData<>(this.getName(),
 			"second motor temp celcius", 0.0);
 
-	protected LoggedMechanism2d mechanism2d = new LoggedMechanism2d(60, 60);
-	protected LoggedMechanismRoot2d armRoot = mechanism2d.getRoot("ArmRoot", 30, 30);
-	protected LoggedMechanismLigament2d armLigament = armRoot.append(new LoggedMechanismLigament2d("Arm", 24, 0));
+	private LoggedMechanism2d mechanism2d = new LoggedMechanism2d(60, 60);
+	private LoggedMechanismRoot2d armRoot = mechanism2d.getRoot("ArmRoot", 30, 30);
+	private LoggedMechanismLigament2d armLigament = armRoot.append(new LoggedMechanismLigament2d("Arm", 24, 0));
 
 	public Arm() {
 	}
