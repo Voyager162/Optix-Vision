6e44a0f5e1af6752202d4f0c3aa0a5715272704c
spooketti <jonliu168@gmail.com>
2025-01-27 13:50:57 -0800

    small stuff not to be worried about

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/ToPosConstants.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.swerve;
 
-import java.security.DrbgParameters.Capability;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
