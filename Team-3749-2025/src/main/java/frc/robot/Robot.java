21722589d6a67dbc4fcdfa745ec88024b4bac3ea
LiliWuu <lilianwu08@gmail.com>
2025-01-28 01:01:07 -0800

    addRequirement

--- Team-3749-2025/src/main/java/frc/robot/Robot.java
+++ Team-3749-2025/src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
@@ -11,6 +12,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.elevator.Elevator;
 
 import frc.robot.subsystems.arm.algae.AlgaeArm;
@@ -31,18 +33,16 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static Swerve swerve = new Swerve();
-  public static Roller algaeRoller = new AlgaeRoller();
-  public static Roller coralRoller = new CoralRoller();
-  public static Roller scoringRoller = new ScoringRoller();
-  
+  public static AlgaeRoller algaeRoller = new AlgaeRoller();
+  public static CoralRoller coralRoller = new CoralRoller();
+  public static ScoringRoller scoringRoller = new ScoringRoller();
+
   public static ExampleSubsystem subsystem = new ExampleSubsystem();
   public static Elevator elevator = new Elevator();
 
   public static AlgaeArm algaeArm = new AlgaeArm();
   public static CoralArm coralArm = new CoralArm();
   public static ClimbArm climbArm = new ClimbArm();
-
-  public static Chute chute = new Chute();
 
   private ShuffleData<Double> batteryVoltageLog = new ShuffleData<Double>("DS", "battery voltage", 0.0);
   private ShuffleData<Boolean> isBrownedOutLog = new ShuffleData<Boolean>("DS", "brownout", false);
@@ -53,10 +53,14 @@ public class Robot extends TimedRobot {
   private ShuffleData<Boolean> FMSLog = new ShuffleData<Boolean>("DS", "FMS connected", false);
   private RobotContainer m_robotContainer;
 
+  public static Subsystem[] getAllSuperStructureSubsystems() {
+    return new Subsystem[] {algaeRoller, coralRoller, scoringRoller, elevator, algaeArm, coralArm, climbArm};
+  }
+
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
-    
+
   }
 
   @Override
@@ -113,7 +117,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    
+
   }
 
   @Override
@@ -132,8 +136,9 @@ public class Robot extends TimedRobot {
   @Override
   public void testExit() {
   }
+
   @Override
-  public void simulationInit(){
+  public void simulationInit() {
     DriverStationSim.setAllianceStationId(AllianceStationID.Blue1);
   }
 }
