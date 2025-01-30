0ed241768e0d186b0b713466f16bc7bc64826fbe
Voyager162 <westonrgardner@gmail.com>
2025-01-30 15:48:49 -0800

    defined the buttonborad

--- Team-3749-2025/src/main/java/frc/robot/utils/JoystickIO.java
+++ Team-3749-2025/src/main/java/frc/robot/utils/JoystickIO.java
@@ -1,9 +1,11 @@
 package frc.robot.utils;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.robot.commands.elevator.SetElevatorState;
 
@@ -37,6 +39,8 @@ public class JoystickIO {
     private static final SetElevatorState l2 = new SetElevatorState(ElevatorStates.L2);
     private static final SetElevatorState l3 = new SetElevatorState(ElevatorStates.L3);
     private static final SetElevatorState l4 = new SetElevatorState(ElevatorStates.L4);
+
+    private final GenericHID buttonBoard = new GenericHID(1);
 
     public JoystickIO() {
     }
