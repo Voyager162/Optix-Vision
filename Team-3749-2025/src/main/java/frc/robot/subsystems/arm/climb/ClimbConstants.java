b124509ca014f7d0998bc467de674f4d7ffc7c04
Voyager162 <westonrgardner@gmail.com>
2025-01-31 11:24:31 -0800

    updated comments for the climb constants file and reorganized it

--- Team-3749-2025/src/main/java/frc/robot/subsystems/arm/climb/ClimbConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/arm/climb/ClimbConstants.java
@@ -8,10 +8,13 @@ import edu.wpi.first.math.util.Units;
  * @author Weston Gardner
  */
 public class ClimbConstants {
+
+	// motors
 	public static final int firstMotorId = 0;
 	public static final int secondMotorId = 1;
 	public static final int numMotors = 2;
 
+	// arm specifications
 	public static final int armLength_inches = 16;
 	public static final double armLength_meters = Units.inchesToMeters(armLength_inches);
 
@@ -22,6 +25,12 @@ public class ClimbConstants {
 	public static final double armMass_kg = 8.44;
 	public static final double armGearing = 255;
 
+	public static final double maxVelocity = 0.0;
+	public static final double maxAcceleration = 0.0;
+
+	public static final double momentOfInertia = 0.775;
+
+	// control values
 	public static double kP = 15;
 	public static final double kI = 0.0;
 	public static final double kD = 0.0;
@@ -31,14 +40,13 @@ public class ClimbConstants {
 	public static final double kA = 0.0;
 	public static final double kV = 0.0;
 
+	// setpoints
 	public static final double stowSetPoint_rad = 0;
 	public static final double PrepareForClimbSetPoint_rad = Math.PI / 2; // 90
 	public static final double climbSetPoint_rad = 20 * Math.PI / 180; // 20
 
-	public static final double maxVelocity = 0.0;
-	public static final double maxAcceleration = 0.0;
 
-	public static final double momentOfInertia = 0.775;
+	// extra
 	public static final boolean simulateGravity = true;
 
 	public enum ArmStates {
