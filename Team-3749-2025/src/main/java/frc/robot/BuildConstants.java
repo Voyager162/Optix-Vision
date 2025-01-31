d9c10fd41d69e7e60cbcaaabec4ab56aaaa7561d
Voyager162 <westonrgardner@gmail.com>
2025-01-30 16:18:31 -0800

    made the build constants file not on the gitignore thing, cause it causes errors

---
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Team-3749-2025";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 235;
+    public static final String GIT_SHA = "f91d77497f3e2ce8ce4708db4602886bdbd023cb";
+    public static final String GIT_DATE = "2025-01-31 00:00:41 GMT";
+    public static final String GIT_BRANCH = "advantage_kit";
+    public static final String BUILD_DATE = "2025-01-31 00:17:53 GMT";
+    public static final long BUILD_UNIX_TIME = 1738282673005L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
