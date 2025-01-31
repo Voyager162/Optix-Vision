2e3b307dcc1afa6041c67ec724afd29a553cdb4d
Voyager162 <westonrgardner@gmail.com>
2025-01-30 16:19:01 -0800

    removed unused imports

--- Team-3749-2025/src/main/java/frc/robot/Robot.java
+++ Team-3749-2025/src/main/java/frc/robot/Robot.java
@@ -9,8 +9,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
