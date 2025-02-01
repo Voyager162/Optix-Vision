ac5b4d2e624b5bdbe4f4043073c9d2bfef891441
Voyager162 <westonrgardner@gmail.com>
2025-02-01 11:56:49 -0800

    build constants stuff

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,14 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 238;
-    public static final String GIT_SHA = "f005f3147dc35d4462533989b7a6b439328e9253";
-    public static final String GIT_DATE = "2025-01-31 00:23:39 GMT";
+    public static final int GIT_REVISION = 240;
+    public static final String GIT_SHA = "72982211b434b67e4c1217b42b4a23b7f6c51b3d";
+    public static final String GIT_DATE = "2025-02-01 19:11:57 GMT";
     public static final String GIT_BRANCH = "advantage_kit";
-    public static final String BUILD_DATE = "2025-01-31 17:39:17 GMT";
-    public static final long BUILD_UNIX_TIME = 1738345157972L;
+    public static final String BUILD_DATE = "2025-02-01 19:56:20 GMT";
+    public static final long BUILD_UNIX_TIME = 1738439780057L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
 }
-// comment
