b9639450b04832345a104039c4cf2088c0a402d4
Voyager162 <westonrgardner@gmail.com>
2025-02-02 14:17:26 -0800

    updated the arm length, the minimum, maxiumum and starting angle as well as the arm mass, all from the cad

--- Team-3749-2025/src/main/java/frc/robot/subsystems/arm/coral/CoralConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/arm/coral/CoralConstants.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.arm.coral;
 
-import edu.wpi.first.math.util.Units;
 
 /**
  * Constants file for the climb arm subsystem
@@ -15,14 +14,13 @@ public class CoralConstants {
 	public static final int numMotors = 1;
 
 	// arm specifications
-	public static final double armLength_inches = 17.796;
-	public static final double armLength_meters = Units.inchesToMeters(armLength_inches);
+	public static final double armLength_meters = 0.442493;
 
-	public static final int armMinAngle_degrees = 10;
-	public static final int armMaxAngle_degrees = 180;
-	public static final int armStartingAngle_degrees = 90;
+	public static final int armMinAngle_degrees = 45;
+	public static final int armMaxAngle_degrees = 240;
+	public static final int armStartingAngle_degrees = 45;
 
-	public static final double armMass_kg = 1.132;
+	public static final double armMass_kg = 2.10899999570037;
 	public static final double armGearing = 40;
 
 	public static final double maxVelocity = 5;
