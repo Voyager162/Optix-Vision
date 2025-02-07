d7773edb82c42eea8de3c79520610b8952f53f6e
Dhyan Soni <dhyantsoni@email.com>
2025-02-06 16:09:41 -0800

    otf now has rotated robot

--- Team-3749-2025/src/main/java/frc/robot/commands/integration/CoralIntakeSource.java
+++ Team-3749-2025/src/main/java/frc/robot/commands/integration/CoralIntakeSource.java
@@ -15,7 +15,7 @@ public class CoralIntakeSource extends Command {
 
     @Override
     public void initialize() {
-        Robot.coralArm.setState(CoralConstants.ArmStates.SOURCE);
+        Robot.coralArm.setState(CoralConstants.ArmStates.CORAL_PICKUP); // this should be source???
         Robot.elevator.setState(ElevatorStates.STOW);
         Robot.chuteRoller.setState(RollerStates.STOP);
         Robot.coralRoller.setState(RollerStates.INTAKE);
--- Team-3749-2025/src/main/java/frc/robot/subsystems/arm/coral/CoralArm.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/arm/coral/CoralArm.java
@@ -4,8 +4,6 @@ import frc.robot.Robot;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.real.ArmSparkMax;
 import frc.robot.subsystems.arm.sim.ArmSim;
-import frc.robot.subsystems.roller.PhotoelectricIO;
-import frc.robot.subsystems.roller.PhotoelectricIO.PhotoelectricData;
 import frc.robot.utils.ShuffleData;
 import frc.robot.utils.UtilityFunctions;
 import edu.wpi.first.math.controller.PIDController;
@@ -32,9 +30,7 @@ import static edu.wpi.first.units.Units.*;
 public class CoralArm extends Arm {
 
     private CoralConstants.ArmStates state = CoralConstants.ArmStates.STOPPED;
-    // private boolean hasPiece;
-    private PhotoelectricData photoelectricData = new PhotoelectricData();
-    private PhotoelectricIO photoelectricIO;
+
     private PIDController controller = new PIDController(
             CoralConstants.kP,
             CoralConstants.kI,
@@ -44,7 +40,8 @@ public class CoralArm extends Arm {
 
     private Mechanism2d mechanism2d = new Mechanism2d(3, 3);
     private MechanismRoot2d armRoot = mechanism2d.getRoot("ArmRoot", 1.8, .4);
-    private MechanismLigament2d armLigament = armRoot.append(new MechanismLigament2d("Coral Arm", CoralConstants.armLength_meters, 0));
+    private MechanismLigament2d armLigament = armRoot
+            .append(new MechanismLigament2d("Coral Arm", CoralConstants.armLength_meters, 0));
     // private Pose3d zeroedComponentPose = new Pose3d(0, 0, 0, new Rotation3d(roll,
     // pitch, yaw));
 
@@ -70,10 +67,9 @@ public class CoralArm extends Arm {
 
         } else {
             armIO = new ArmSparkMax(CoralConstants.motorId);
-            // this.photoelectricIO = new JTVisiSight();
         }
         SmartDashboard.putData("Coral Arm Mechanism", mechanism2d);
-        // System.out.println(data.positionUnits);
+        System.out.println(data.positionUnits);
     }
 
     /**
@@ -95,26 +91,19 @@ public class CoralArm extends Arm {
 
         switch (state) {
             case STOWED:
-                return UtilityFunctions.withinMargin(0.01, CoralConstants.stowSetPoint_rad,
-                        data.positionUnits);
+                return data.positionUnits == CoralConstants.stowSetPoint_rad;
             case HAND_OFF:
-                return UtilityFunctions.withinMargin(0.01, CoralConstants.handOffSetPoint_rad,
-                        data.positionUnits);
+                return data.positionUnits == CoralConstants.handOffSetPoint_rad;
             case CORAL_PICKUP:
-                return UtilityFunctions.withinMargin(0.01, CoralConstants.coralPickUpSetPoint_rad,
-                        data.positionUnits);
-            case L1:
-                return UtilityFunctions.withinMargin(0.01, CoralConstants.L1SetPoint_rad,
-                        data.positionUnits);
-            case SOURCE:
-                return UtilityFunctions.withinMargin(0.01, CoralConstants.sourceSetPoint_rad,
-                        data.positionUnits);
+                return data.positionUnits == CoralConstants.coralPickUpSetPoint_rad;
             case MOVING_DOWN:
                 return data.velocityUnits < 0;
             case MOVING_UP:
                 return data.velocityUnits > 0;
+            case L1:
+                return data.positionUnits == CoralConstants.scoreL1_rad;
             case STOPPED:
-                return UtilityFunctions.withinMargin(0.01, 0, data.velocityUnits);
+                return UtilityFunctions.withinMargin(0.001, 0, data.velocityUnits);
             default:
                 return false;
         }
@@ -146,14 +135,9 @@ public class CoralArm extends Arm {
             case CORAL_PICKUP:
                 setVoltage(controller.calculate(data.positionUnits, CoralConstants.coralPickUpSetPoint_rad)
                         + calculateFeedForward());
-
                 break;
             case L1:
-                setVoltage(controller.calculate(data.positionUnits, CoralConstants.L1SetPoint_rad)
-                        + calculateFeedForward());
-                break;
-            case SOURCE:
-                setVoltage(controller.calculate(data.positionUnits, CoralConstants.sourceSetPoint_rad)
+                setVoltage(controller.calculate(data.positionUnits, CoralConstants.scoreL1_rad)
                         + calculateFeedForward());
                 break;
             case STOPPED:
@@ -200,14 +184,15 @@ public class CoralArm extends Arm {
     }
 
     private Angle getPitch() {
-        // System.out.println(data.positionUnits);
+        System.out.println("Coral Arm: " + data.positionUnits);
         return Angle.ofBaseUnits(data.positionUnits + Units.degreesToRadians(-55), Radians); // remove offset once coral
                                                                                              // arm code is fixed
     }
 
     private Transform3d getTransform3d() {
-        Transform3d transform = new Transform3d(0.285, 0.01, 0.4,
-                new Rotation3d(Angle.ofBaseUnits(0, Radians), getPitch(), Angle.ofBaseUnits(0, Radians)));
+        Transform3d transform = new Transform3d(0, 0.35, 0.4,
+                new Rotation3d(Angle.ofBaseUnits(0, Radians), getPitch(),
+                        Angle.ofBaseUnits(Units.degreesToRadians(90), Radians)));
         return transform;
     }
 
@@ -216,44 +201,16 @@ public class CoralArm extends Arm {
         return feedForward;
     }
 
-    // public boolean hasPiece() {
-    //     return photoelectricData.sensing;
-    //     // return hasPiece;
-    // }
-
-    // public void setHasPiece(boolean hasPiece) {
-    //     this.hasPiece = hasPiece;
-    // }
-
-    public PhotoelectricIO getPhotoElectricIO() {
-        return photoelectricIO;
-    }
-
     /**
      * Periodic method for updating arm behavior.
      */
     @Override
     public void periodic() {
-        super.periodic();
         armIO.updateData(data);
 
         logData();
+
         runState();
-
-        // Ensure photoelectricData is updated
-
-        // photoelectricIO.updateData(photoelectricData);
-
-        // if (Robot.isSimulation()) {
-        //     if (this.getCurrentCommand() != null) {
-        //         photoelectricIO.setSensing(this.getCurrentCommand().getName());
-        //         // Update hasPiece based on sensing
-        //         // setHasPiece(photoelectricData.sensing);
-
-        //         // Debugging logs
-        //         // System.out.println("Command: " + this.getCurrentCommand().getName());
-        //         // System.out.println("Sensing: " + photoelectricData.sensing);
-        //     }
-        // }
     }
+
 }
