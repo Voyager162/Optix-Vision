0fcd76b57383f2e67d793c23db7f22ffce7d866f
Voyager162 <westonrgardner@gmail.com>
2025-02-10 14:34:57 -0800

    did the same for the rollers

--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/implementations/CoralRoller.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/implementations/CoralRoller.java
@@ -4,29 +4,15 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import frc.robot.subsystems.roller.Roller;
 import frc.robot.subsystems.roller.RollerConstants;
 import frc.robot.subsystems.roller.RollerConstants.Implementations;
-import frc.robot.utils.LoggedTunableNumber;
-
 public class CoralRoller extends Roller {
 
     public CoralRoller() {
         super(Implementations.CORAL, FF());
-
-        ks = new LoggedTunableNumber(getName() + "/kS", RollerConstants.Coral.kSVelocity);
-        kv = new LoggedTunableNumber(getName() + "/kV", RollerConstants.Coral.kVVelocity);
-        ka = new LoggedTunableNumber(getName() + "/kA", RollerConstants.Coral.kAVelocity);
-
-        RollerConstants.Coral.kSVelocity = ks.get();
-        RollerConstants.Coral.kVVelocity = kv.get();
-        RollerConstants.Coral.kAVelocity = ka.get();
-
-        maxVelocity = new LoggedTunableNumber(getName() + "/maxVelocity", RollerConstants.Coral.maxVelocity);
-        maxAcceleration = new LoggedTunableNumber(getName() + "/maxAcceleration",
-                RollerConstants.Coral.maxAcceleration);
     }
 
     public static SimpleMotorFeedforward FF() {
-        return new SimpleMotorFeedforward(RollerConstants.Coral.kSVelocity, RollerConstants.Coral.kVVelocity,
-                RollerConstants.Coral.kAVelocity);
+        return new SimpleMotorFeedforward(RollerConstants.Coral.kSVelocity.get(), RollerConstants.Coral.kVVelocity.get(),
+                RollerConstants.Coral.kAVelocity.get());
     }
 
     @Override
--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/real/RollerSparkMax.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/real/RollerSparkMax.java
@@ -24,70 +24,30 @@ public class RollerSparkMax implements RollerIO {
 
         switch (implementation) {
             case ALGAE:
-                kp = new LoggedTunableNumber("AlgaeRoller" + "/kP", Algae.kPVelocity);
-                ki = new LoggedTunableNumber("AlgaeRoller" + "/kI", Algae.kIVelocity);
-                kd = new LoggedTunableNumber("AlgaeRoller" + "/kD", Algae.kDVelocity);
-                Algae.kPVelocity = kp.get();
-                Algae.kIVelocity = ki.get();
-                Algae.kDVelocity = kd.get();
-
-                kp = new LoggedTunableNumber("AlgaeRoller" + "/kP", Algae.kPPosition);
-                ki = new LoggedTunableNumber("AlgaeRoller" + "/kI", Algae.kIPosition);
-                kd = new LoggedTunableNumber("AlgaeRoller" + "/kD", Algae.kDPosition);
-                Algae.kPPosition = kp.get();
-                Algae.kIPosition = ki.get();
-                Algae.kDPosition = kd.get();
 
                 rollerMotor = new OptixSpark(Algae.motorId, OptixSpark.Type.SPARKMAX);
                 rollerMotor.setPositionConversionFactor(2 * Math.PI / Algae.gearRatio);
                 rollerMotor.setVelocityConversionFactor((2 * Math.PI / Algae.gearRatio) / 60.0);
                 rollerMotor.setInverted(Algae.inverted);
-                rollerMotor.setPID(Algae.kPPosition, Algae.kIPosition, Algae.kDPosition, ClosedLoopSlot.kSlot0);
-                rollerMotor.setPID(Algae.kPVelocity, Algae.kIVelocity, Algae.kDVelocity, ClosedLoopSlot.kSlot1);
+                rollerMotor.setPID(Algae.kPPosition.get(), Algae.kIPosition.get(), Algae.kDPosition.get(), ClosedLoopSlot.kSlot0);
+                rollerMotor.setPID(Algae.kPVelocity.get(), Algae.kIVelocity.get(), Algae.kDVelocity.get(), ClosedLoopSlot.kSlot1);
                 break;
             case SCORING:
-                kp = new LoggedTunableNumber("ScoringRoller" + "/kP", Scoring.kPVelocity);
-                ki = new LoggedTunableNumber("ScoringRoller" + "/kI", Scoring.kIVelocity);
-                kd = new LoggedTunableNumber("ScoringRoller" + "/kD", Scoring.kDVelocity);
-                Scoring.kPVelocity = kp.get();
-                Scoring.kIVelocity = ki.get();
-                Scoring.kDVelocity = kd.get();
-
-                kp = new LoggedTunableNumber("ScoringRoller" + "/kP", Scoring.kPPosition);
-                ki = new LoggedTunableNumber("ScoringRoller" + "/kI", Scoring.kIPosition);
-                kd = new LoggedTunableNumber("ScoringRoller" + "/kD", Scoring.kDPosition);
-                Scoring.kPPosition = kp.get();
-                Scoring.kIPosition = ki.get();
-                Scoring.kDPosition = kd.get();
 
                 rollerMotor = new OptixSpark(Scoring.motorId, OptixSpark.Type.SPARKMAX);
                 rollerMotor.setPositionConversionFactor(2 * Math.PI / Scoring.gearRatio);
                 rollerMotor.setVelocityConversionFactor((2 * Math.PI / Scoring.gearRatio) / 60.0);
                 rollerMotor.setInverted(Scoring.inverted);
-                rollerMotor.setPID(Scoring.kPPosition, Scoring.kIPosition, Scoring.kDPosition, ClosedLoopSlot.kSlot0);
-                rollerMotor.setPID(Scoring.kPVelocity, Scoring.kIVelocity, Scoring.kDVelocity, ClosedLoopSlot.kSlot1);
+                rollerMotor.setPID(Scoring.kPPosition.get(), Scoring.kIPosition.get(), Scoring.kDPosition.get(), ClosedLoopSlot.kSlot0);
+                rollerMotor.setPID(Scoring.kPVelocity.get(), Scoring.kIVelocity.get(), Scoring.kDVelocity.get(), ClosedLoopSlot.kSlot1);
                 break;
             case CORAL:
-                kp = new LoggedTunableNumber("CoralRoller" + "/kP", Coral.kPVelocity);
-                ki = new LoggedTunableNumber("CoralRoller" + "/kI", Coral.kIVelocity);
-                kd = new LoggedTunableNumber("CoralRoller" + "/kD", Coral.kDVelocity);
-                Scoring.kPVelocity = kp.get();
-                Scoring.kIVelocity = ki.get();
-                Scoring.kDVelocity = kd.get();
-
-                kp = new LoggedTunableNumber("CoralRoller" + "/kP", Coral.kPPosition);
-                ki = new LoggedTunableNumber("CoralRoller" + "/kI", Coral.kIPosition);
-                kd = new LoggedTunableNumber("CoralRoller" + "/kD", Coral.kDPosition);
-                Scoring.kPPosition = kp.get();
-                Scoring.kIPosition = ki.get();
-                Scoring.kDPosition = kd.get();
-
                 rollerMotor = new OptixSpark(Coral.motorId, OptixSpark.Type.SPARKMAX);
                 rollerMotor.setPositionConversionFactor(2 * Math.PI / Coral.gearRatio);
                 rollerMotor.setVelocityConversionFactor((2 * Math.PI / Coral.gearRatio) / 60.0);
                 rollerMotor.setInverted(Coral.inverted);
-                rollerMotor.setPID(Coral.kPPosition, Coral.kIPosition, Coral.kDPosition, ClosedLoopSlot.kSlot0);
-                rollerMotor.setPID(Coral.kPVelocity, Coral.kIVelocity, Coral.kDVelocity, ClosedLoopSlot.kSlot1);
+                rollerMotor.setPID(Coral.kPPosition.get(), Coral.kIPosition.get(), Coral.kDPosition.get(), ClosedLoopSlot.kSlot0);
+                rollerMotor.setPID(Coral.kPVelocity.get(), Coral.kIVelocity.get(), Coral.kDVelocity.get(), ClosedLoopSlot.kSlot1);
                 break;
 
             default:
