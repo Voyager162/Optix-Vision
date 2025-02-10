f62ecfb77154fd08ad59183daeb3fd1b662cb515
Voyager162 <westonrgardner@gmail.com>
2025-02-10 11:34:00 -0800

    added build constants to git ignore

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 441;
-    public static final String GIT_SHA = "2318229b3f401f221a65936ce499a224bb6a9d64";
-    public static final String GIT_DATE = "2025-02-08 21:34:02 GMT";
+    public static final int GIT_REVISION = 447;
+    public static final String GIT_SHA = "9805aaf705782b4a6a6deff33eaeb5546c1fdda7";
+    public static final String GIT_DATE = "2025-02-10 19:28:17 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-09 16:59:58 GMT";
-    public static final long BUILD_UNIX_TIME = 1739120398941L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2025-02-10 19:31:26 GMT";
+    public static final long BUILD_UNIX_TIME = 1739215886705L;
+    public static final int DIRTY = 1;
 
     private BuildConstants(){}
 }
