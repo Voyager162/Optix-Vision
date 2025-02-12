07da7c828471eb7cca48ce8bd6e7e0193e509c49
Voyager162 <westonrgardner@gmail.com>
2025-02-11 17:42:21 -0800

    made swerve logged tunable numbers for pid

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -28,14 +28,28 @@ public class SwerveConstants {
                                 { 0.27, 0, 0 }, // no slow velocity control
                                 { 0.27, 0, 0 } }; // no fast velocity control
 
+                // pid
+                public static LoggedTunableNumber largeErrorTurningKP = new LoggedTunableNumber(
+                                "subsystems/swerve/largeErrorTurningKP", 8);
+                public static LoggedTunableNumber smallErrorTurningKP = new LoggedTunableNumber(
+                                "subsystems/swerve/smallErrorTurningKP", 12);
+                public static LoggedTunableNumber noSlowVelocityControlDriveKP = new LoggedTunableNumber(
+                                "subsystems/swerve/noSlowVelocityControlDriveKP", .27);
+                public static LoggedTunableNumber noFastVelocityControlDriveKP = new LoggedTunableNumber(
+                                "subsystems/swerve/noFastVelocityControlDriveKP", .27);
+
                 // our FF values
+                public static LoggedTunableNumber kSDriving = new LoggedTunableNumber("subsystems/swerve/kSDriving",
+                                0.26);
+                public static LoggedTunableNumber kVDriving = new LoggedTunableNumber("subsystems/swerve/kVDriving",
+                                2.765);
+                public static LoggedTunableNumber kADriving = new LoggedTunableNumber("subsystems/swerve/kADriving",
+                                0.0);
 
-                public static LoggedTunableNumber kSDriving = new LoggedTunableNumber("subsystems/swerve/kSDriving", 0.26);
-                public static LoggedTunableNumber kVDriving = new LoggedTunableNumber("subsystems/swerve/kVDriving", 2.765);
-                public static LoggedTunableNumber kADriving = new LoggedTunableNumber("subsystems/swerve/kADriving", 0.0);
-
-                public static LoggedTunableNumber maxVelocity = new LoggedTunableNumber("subsystems/swerve/maxVelocity", 4.3);
-                public static LoggedTunableNumber maxAcceleration = new LoggedTunableNumber("subsystems/swerve/maxAcceleration", 3.3);
+                public static LoggedTunableNumber maxVelocity = new LoggedTunableNumber("subsystems/swerve/maxVelocity",
+                                4.3);
+                public static LoggedTunableNumber maxAcceleration = new LoggedTunableNumber(
+                                "subsystems/swerve/maxAcceleration", 3.3);
 
                 // teleop speed
                 public static final double teleopMaxSpeedReduction = 0; // If we can drive a little faster in telop
