f8b537f4191f08f187bde58b7ee2d4c41a10a913
NoahSimon8 <noah.simon9806@gmail.com>
2025-02-13 00:55:55 -0800

    changed build constants

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 461;
-    public static final String GIT_SHA = "a187fe61193edfd702bc72bb881cb9b8ce4c410b";
-    public static final String GIT_DATE = "2025-02-10 23:45:40 GMT";
-    public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-11 01:32:05 GMT";
-    public static final long BUILD_UNIX_TIME = 1739237525107L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 519;
+    public static final String GIT_SHA = "2014ce1136e06c5c69d86d4e7c3e56e3704a71ba";
+    public static final String GIT_DATE = "2025-02-13 08:53:45 GMT";
+    public static final String GIT_BRANCH = "advantage_kit";
+    public static final String BUILD_DATE = "2025-02-13 08:55:35 GMT";
+    public static final long BUILD_UNIX_TIME = 1739436935555L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
