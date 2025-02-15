672121802925ec4dffb39351f5f27cb1bdb86be0
Voyager162 <westonrgardner@gmail.com>
2025-02-15 12:46:30 -0800

    ks and max velocity for coral roller tuned

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 477;
-    public static final String GIT_SHA = "af837aae7ac29d9bdc450f48d1368d2a107c7b27";
-    public static final String GIT_DATE = "2025-02-15 08:54:56 GMT";
+    public static final int GIT_REVISION = 478;
+    public static final String GIT_SHA = "039563a4fb4845b06e17f81fe1bf00485905977d";
+    public static final String GIT_DATE = "2025-02-15 20:22:43 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-15 20:20:12 GMT";
-    public static final long BUILD_UNIX_TIME = 1739650812714L;
+    public static final String BUILD_DATE = "2025-02-15 20:44:20 GMT";
+    public static final long BUILD_UNIX_TIME = 1739652260590L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
