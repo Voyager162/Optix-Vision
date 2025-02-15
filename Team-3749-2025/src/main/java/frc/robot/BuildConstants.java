63e2e6e071327b6bb1c5c6590644c9e5aae46d12
NoahSimon8 <noah.simon9806@gmail.com>
2025-02-15 14:38:02 -0800

    swerve encoder offsets

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 476;
-    public static final String GIT_SHA = "95c7a2dfda097f127c59a359faef9205e5a6d907";
-    public static final String GIT_DATE = "2025-02-15 08:31:39 GMT";
+    public static final int GIT_REVISION = 477;
+    public static final String GIT_SHA = "af837aae7ac29d9bdc450f48d1368d2a107c7b27";
+    public static final String GIT_DATE = "2025-02-15 08:54:56 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-15 08:51:13 GMT";
-    public static final long BUILD_UNIX_TIME = 1739609473290L;
+    public static final String BUILD_DATE = "2025-02-15 22:22:52 GMT";
+    public static final long BUILD_UNIX_TIME = 1739658172063L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
