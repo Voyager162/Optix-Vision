ddd4bb15fd3d48c45d49171b02bb2c14495bccd0
Voyager162 <westonrgardner@gmail.com>
2025-02-15 16:55:25 -0800

    fixed the algae roller gear ratio

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 489;
-    public static final String GIT_SHA = "4fd9f561af937e663b4f58cc8dba93fc8cae73d1";
-    public static final String GIT_DATE = "2025-02-15 23:19:13 GMT";
+    public static final int GIT_REVISION = 490;
+    public static final String GIT_SHA = "dea760b51ec6a6078993f4b2ad726579705c7401";
+    public static final String GIT_DATE = "2025-02-16 00:20:03 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-16 00:16:43 GMT";
-    public static final long BUILD_UNIX_TIME = 1739665003187L;
+    public static final String BUILD_DATE = "2025-02-16 00:22:45 GMT";
+    public static final long BUILD_UNIX_TIME = 1739665365447L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/roller/RollerConstants.java
@@ -8,7 +8,7 @@ public class RollerConstants {
         public static final boolean inverted = false;
 
         public static final double momentOfInertia = 0.04;
-        public static final double gearRatio = 3.0;
+        public static final double gearRatio = 4.0;
         public static final double measurementNoise = 0.0;
 
         public static LoggedTunableNumber kPVelocity = new LoggedTunableNumber("/subsystems/rollers/algaeRoller/kPVelocity", 0.0);
