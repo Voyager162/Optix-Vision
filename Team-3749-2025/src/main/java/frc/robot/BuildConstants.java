a594f39242630bac784e44e41f83040927ab9531
Harkirat47 <har.ki.kirat@gmail.com>
2025-02-16 12:14:59 -0800

    tuner

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 994;
-    public static final String GIT_SHA = "a346def07bae13f96dafda4bdbe3cb779e825788";
-    public static final String GIT_DATE = "2025-02-16 19:20:43 GMT";
+    public static final int GIT_REVISION = 997;
+    public static final String GIT_SHA = "5c7e346125856fa890cb3769c81f24eb456dc0a8";
+    public static final String GIT_DATE = "2025-02-16 20:01:50 GMT";
     public static final String GIT_BRANCH = "otf_pathing";
-    public static final String BUILD_DATE = "2025-02-16 19:48:23 GMT";
-    public static final long BUILD_UNIX_TIME = 1739735303104L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-02-16 20:02:04 GMT";
+    public static final long BUILD_UNIX_TIME = 1739736124748L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
