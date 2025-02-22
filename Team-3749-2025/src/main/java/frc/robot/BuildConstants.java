a6b1a423d06de7957b82318c9803bbda691047ff
Voyager162 <westonrgardner@gmail.com>
2025-02-22 13:01:36 -0800

    new button bindings for the button board

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1073;
-    public static final String GIT_SHA = "c2f4c65848afe7991c7535a312a3db002502c5ad";
-    public static final String GIT_DATE = "2025-02-22 12:19:59 GMT";
+    public static final int GIT_REVISION = 1075;
+    public static final String GIT_SHA = "f327eb171d615cb88b7e9157bd9e39620f5ccb1f";
+    public static final String GIT_DATE = "2025-02-22 19:04:52 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-22 12:51:04 GMT";
-    public static final long BUILD_UNIX_TIME = 1740228664757L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-02-22 20:39:57 GMT";
+    public static final long BUILD_UNIX_TIME = 1740256797601L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
--- Team-3749-2025/src/main/java/frc/robot/buttons/ButtonBoard.java
+++ Team-3749-2025/src/main/java/frc/robot/buttons/ButtonBoard.java
@@ -10,34 +10,34 @@ public class ButtonBoard {
     public GenericHID buttonBoardPlayer3 = new GenericHID(3);
 
     // coral elevator positions
-    public JoystickButton buttonl1 = new JoystickButton(buttonBoardPlayer3, 2);
-    public JoystickButton buttonl2 = new JoystickButton(buttonBoardPlayer3, 1);
-    public JoystickButton buttonl3 = new JoystickButton(buttonBoardPlayer1, 6);
-    public JoystickButton buttonl4 = new JoystickButton(buttonBoardPlayer1, 5);
+    public JoystickButton buttonl1 = new JoystickButton(buttonBoardPlayer3, 6);
+    public JoystickButton buttonl2 = new JoystickButton(buttonBoardPlayer3, 5);
+    public JoystickButton buttonl3 = new JoystickButton(buttonBoardPlayer1,4);
+    public JoystickButton buttonl4 = new JoystickButton(buttonBoardPlayer1, 3);
 
     // source buttons
-    public JoystickButton buttonRightSource = new JoystickButton(buttonBoardPlayer1, 4);
-    public JoystickButton buttonLeftSource = new JoystickButton(buttonBoardPlayer1, 7);
+    public JoystickButton buttonRightSource = new JoystickButton(buttonBoardPlayer1, 2);
+    public JoystickButton buttonLeftSource = new JoystickButton(buttonBoardPlayer1, 1);
 
     // reef positions
-    public JoystickButton buttonReefZoneA = new JoystickButton(buttonBoardPlayer3, 6);
-    public JoystickButton buttonReefZoneB = new JoystickButton(buttonBoardPlayer2, 5);
-    public JoystickButton buttonReefZoneC = new JoystickButton(buttonBoardPlayer2, 6);
-    public JoystickButton buttonReefZoneD = new JoystickButton(buttonBoardPlayer3, 3);
-    public JoystickButton buttonReefZoneE = new JoystickButton(buttonBoardPlayer3, 4);
-    public JoystickButton buttonReefZoneF = new JoystickButton(buttonBoardPlayer2, 8);
-    public JoystickButton buttonReefZoneG = new JoystickButton(buttonBoardPlayer2, 7);
-    public JoystickButton buttonReefZoneH = new JoystickButton(buttonBoardPlayer2, 4);
-    public JoystickButton buttonReefZoneI = new JoystickButton(buttonBoardPlayer2, 3);
-    public JoystickButton buttonReefZoneJ = new JoystickButton(buttonBoardPlayer2, 2);
-    public JoystickButton buttonReefZoneK = new JoystickButton(buttonBoardPlayer2, 1);
-    public JoystickButton buttonReefZoneL = new JoystickButton(buttonBoardPlayer3, 5);
+    public JoystickButton buttonReefZoneLeft1 = new JoystickButton(buttonBoardPlayer2, 1);
+    public JoystickButton buttonReefZoneLeft2 = new JoystickButton(buttonBoardPlayer1, 7);
+    public JoystickButton buttonReefZoneLeft3 = new JoystickButton(buttonBoardPlayer2, 3);
+    public JoystickButton buttonReefZoneLeft4 = new JoystickButton(buttonBoardPlayer2, 4);
+    public JoystickButton buttonReefZoneLeft5 = new JoystickButton(buttonBoardPlayer2, 5);
+    public JoystickButton buttonReefZoneLeft6 = new JoystickButton(buttonBoardPlayer2, 6);
+    public JoystickButton buttonReefZoneRight1 = new JoystickButton(buttonBoardPlayer3, 4);
+    public JoystickButton buttonReefZoneRight2 = new JoystickButton(buttonBoardPlayer3,3);
+    public JoystickButton buttonReefZoneRight3 = new JoystickButton(buttonBoardPlayer3, 2);
+    public JoystickButton buttonReefZoneRight4 = new JoystickButton(buttonBoardPlayer3, 1);
+    public JoystickButton buttonReefZoneRight5 = new JoystickButton(buttonBoardPlayer2, 8);
+    public JoystickButton buttonReefZoneRight6 = new JoystickButton(buttonBoardPlayer2, 7);
 
     // miscellaneous buttons
-    public JoystickButton buttonAlgaeKnockoff = new JoystickButton(buttonBoardPlayer1, 3);
-    public JoystickButton buttonUtilityA = new JoystickButton(buttonBoardPlayer1, 1);
-    public JoystickButton buttonUtilityB = new JoystickButton(buttonBoardPlayer1, 2);
-    public JoystickButton buttonPlayer1Start = new JoystickButton(buttonBoardPlayer1, 8);
+    public JoystickButton buttonAlgaeKnockoff = new JoystickButton(buttonBoardPlayer3, 8);
+    public JoystickButton buttonUtilityA = new JoystickButton(buttonBoardPlayer3, 7);
+    public JoystickButton buttonUtilityB = new JoystickButton(buttonBoardPlayer3, 6);
+    public JoystickButton buttonReset = new JoystickButton(buttonBoardPlayer3, 5);
 
     public ButtonBoard() {
     }
