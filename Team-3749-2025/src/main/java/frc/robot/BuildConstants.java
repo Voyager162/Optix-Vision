866b27c88afead3269d2b1d4c1d4fce288d9cbf7
LiliWuu <lilianwu08@gmail.com>
2025-02-22 11:48:54 -0800

    renaming traj in auto

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1074;
-    public static final String GIT_SHA = "cdb23a9e9c0362fdca59afdc2f46dbf1dc495ce8";
-    public static final String GIT_DATE = "2025-02-22 12:52:45 GMT";
+    public static final int GIT_REVISION = 1078;
+    public static final String GIT_SHA = "104ff755fc5ee0bd215be5f64cae9d1d06141bd3";
+    public static final String GIT_DATE = "2025-02-22 19:32:07 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-22 19:22:09 GMT";
-    public static final long BUILD_UNIX_TIME = 1740252129111L;
+    public static final String BUILD_DATE = "2025-02-22 19:47:29 GMT";
+    public static final long BUILD_UNIX_TIME = 1740253649791L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
