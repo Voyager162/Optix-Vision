9f1a728cb3dda949d1ead36306502ded691db8b6
NoahSimon8 <noah.simon9806@gmail.com>
2025-02-24 12:35:25 -0800

    swerve kinematics and tuning (with drive current error)

--- Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Team-3749-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1100;
-    public static final String GIT_SHA = "0c3a3a72a5fb6ec9de69567342ffc6f196036209";
-    public static final String GIT_DATE = "2025-02-23 20:54:32 GMT";
+    public static final int GIT_REVISION = 1101;
+    public static final String GIT_SHA = "39081a22758a56a9d9c2e46cf6949ae1343783d2";
+    public static final String GIT_DATE = "2025-02-23 21:06:56 GMT";
     public static final String GIT_BRANCH = "tuning";
-    public static final String BUILD_DATE = "2025-02-23 20:58:57 GMT";
-    public static final long BUILD_UNIX_TIME = 1740344337238L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-02-23 23:34:24 GMT";
+    public static final long BUILD_UNIX_TIME = 1740353664620L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
