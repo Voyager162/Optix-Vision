b758ff6dd3e6b436e1084acbcdd89c6979d15479
NoahSimon8 <noah.simon9806@gmail.com>
2025-02-27 01:00:09 -0800

    gyro config

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/real/PigeonGyro.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/real/PigeonGyro.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.swerve.real;
 
+import com.ctre.phoenix6.configs.GyroTrimConfigs;
+import com.ctre.phoenix6.configs.MountPoseConfigs;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import frc.robot.subsystems.swerve.GyroIO;
 
@@ -10,14 +13,22 @@ import frc.robot.subsystems.swerve.GyroIO;
  */
 public class PigeonGyro implements GyroIO {
     private final Pigeon2 pigeonGyro = new Pigeon2(30);
+    private final Pigeon2Configuration config = new Pigeon2Configuration();
+    private final GyroTrimConfigs trimConfig = new GyroTrimConfigs();
+    private final MountPoseConfigs mountConfig = new MountPoseConfigs();
     private double yaw = 0;
 
     public PigeonGyro() {
+        mountConfig.withMountPosePitch(0);
+        mountConfig.withMountPoseYaw(90);
+        mountConfig.withMountPoseRoll(-90);
+        trimConfig.withGyroScalarX(-1.21);
+        config.withGyroTrim(trimConfig);
+        config.withMountPose(mountConfig);
 
-        try {
-            pigeonGyro.reset();
-        } catch (Exception e) {
-        }
+        pigeonGyro.reset();
+        pigeonGyro.getConfigurator().apply(config);
+
     }
 
     @Override
@@ -29,10 +40,10 @@ public class PigeonGyro implements GyroIO {
                 yaw -= 360;
             }
             while (yaw < 0) {
-                yaw+=360;
+                yaw += 360;
             }
 
-            data.yawDeg = pigeonGyro.getYaw().getValueAsDouble() ;
+            data.yawDeg = pigeonGyro.getYaw().getValueAsDouble();
             data.pitchDeg = pigeonGyro.getPitch().getValueAsDouble();
             data.rollDeg = pigeonGyro.getRoll().getValueAsDouble();
             data.isConnected = pigeonGyro.isConnected();
