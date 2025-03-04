cc59e5284c5acff2efb9e96d40e2394f181cebbd
NoahSimon8 <noah.simon9806@gmail.com>
2025-03-03 23:15:18 -0800

    changed max acc for bumpers

--- Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -32,7 +32,7 @@ public class SwerveConstants {
                 public static LoggedTunableNumber maxVelocity = new LoggedTunableNumber("swerve/maxVelocity",
                                 4.6);
                 public static LoggedTunableNumber maxAcceleration = new LoggedTunableNumber(
-                                "swerve/maxAcceleration", 3.25); // should be higher
+                                "swerve/maxAcceleration", 3); 
 
                 // teleop speed
                 public static final double teleopMaxSpeedReduction = -0.1; // If we can drive a little faster in telop
