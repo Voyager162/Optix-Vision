faa2e2cdd410819474d3672bc4a599e3cb0bf2f0
NoahSimon8 <noah.simon9806@gmail.com>
2025-03-05 23:17:23 -0800

    deleted mine

--- Team-3749-2025/src/main/java/frc/robot/subsystems/leds/LedTriggers.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/leds/LedTriggers.java
@@ -11,28 +11,23 @@ public class LedTriggers {
 
     public static void createLEDTriggers() {
         Trigger coralHasPieceTrigger = new Trigger(() -> Robot.coralRoller.hasPiece());
-        coralHasPieceTrigger.onTrue(Commands.runOnce(() -> 
-        Robot.led.setLEDStatusIndicator(StatusIndicator.PIECE)));
+        coralHasPieceTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.PIECE)));
 
         Trigger scoringHasPieceTrigger = new Trigger(() -> Robot.scoringRoller.hasPiece());
-        scoringHasPieceTrigger.onTrue(Commands.runOnce(() -> 
-        Robot.led.setLEDStatusIndicator(StatusIndicator.PIECE)));
+        scoringHasPieceTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.PIECE)));
 
         Trigger algaeKnockTrigger = new Trigger(() -> Robot.scoringRoller.getIsAlgaeMode());
-        algaeKnockTrigger.onTrue(Commands.runOnce(() -> 
-        Robot.led.setLEDStatusIndicator(StatusIndicator.ALGAE)));
+        algaeKnockTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.ALGAE)));
 
         Trigger scoringTrigger = new Trigger(() -> Robot.scoringRoller.getState().equals(RollerStates.SCORE));
         scoringTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.SCORING)));
 
         Trigger OTFTrigger = new Trigger(() -> Robot.swerve.getIsOTF());
-        OTFTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.OTF)
-        ));
+        OTFTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.OTF)));
 
         Trigger climbTrigger = new Trigger(() -> Robot.climbArm.getState().equals(ArmStates.CLIMB));
-        climbTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.CLIMB)
-        ));
+        climbTrigger.onTrue(Commands.runOnce(() -> Robot.led.setLEDStatusIndicator(StatusIndicator.CLIMB)));
 
     }
-    
+
 }
