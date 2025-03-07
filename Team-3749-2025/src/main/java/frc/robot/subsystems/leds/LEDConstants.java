fd0b2d6f1b57f96f73ce50b2e915aaf397f37086
NoahSimon8 <noah.simon9806@gmail.com>
2025-03-06 18:57:02 -0800

    LED Colors and tiggers by Drive Team

--- Team-3749-2025/src/main/java/frc/robot/subsystems/leds/LEDConstants.java
+++ Team-3749-2025/src/main/java/frc/robot/subsystems/leds/LEDConstants.java
@@ -19,18 +19,25 @@ public class LEDConstants {
     }
 
     public static enum LEDColor {
-        RED_ALLIANCE(Color.kRed),
-        BLUE_ALLIANCE(Color.kBlue),
+        RED_ALLIANCE(Color.kBlue),
+        BLUE_ALLIANCE(Color.kRed),
         CLIMB(Color.kDarkGreen),
-        SCORING(Color.kDeepPink),
+        // SCORING(Color.kDeepPink),
         NO_TEAM(Color.kWhite),
-        CHUTE_HAS_PIECE(Color.kGreen),
-        CORAL_ARM_HAS_PIECE(Color.kPurple),
+        // CHUTE_HAS_PIECE(Color.kGreen),
+        CORAL_ARM_HAS_PIECE(Color.kGreen),
         OFF(Color.kBlack),
         BATTERY_LOW(Color.kOrange),
         BATTERY_GOOD(Color.kWhite),
-        ALGAE(Color.kYellow),
-        RAINBOW(Color.kTomato); //this is not the actual color i just need to satisfy reqs
+        RAINBOW(Color.kTomato), //this is not the actual color i just need to satisfy reqs
+
+        L1(Color.kPurple),
+        L2(Color.kLightBlue),
+        L3(Color.kGold),
+        L4(Color.kMaroon),
+
+        ALGAE(Color.kAquamarine);
+
 
         Color color;
         boolean isRainbow;
