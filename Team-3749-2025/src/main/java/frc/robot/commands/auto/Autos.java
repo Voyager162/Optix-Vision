f83b410c0b2bd02692ccff5f1b7e68a7fbcac676
spooketti <jonliu168@gmail.com>
2025-03-16 18:38:41 -0700

    cleaning

--- Team-3749-2025/src/main/java/frc/robot/commands/auto/Autos.java
+++ Team-3749-2025/src/main/java/frc/robot/commands/auto/Autos.java
@@ -2,7 +2,6 @@ package frc.robot.commands.auto;
 
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
